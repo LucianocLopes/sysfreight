d209f9453d38b2a744a7ac57c894e657de026b8d
LucianoCLopes <luciano_clopes@hotmail.com>
2024-11-14 12:47:42 -0300

    new corrections templates

--- core/views.py
+++ core/views.py
@@ -1,5 +1,21 @@
 from django.views.generic.base import TemplateView
 
 
-class IndexView(TemplateView):
+class PageTitleViewMixin:
+    title = ""
+
+    def get_title(self):
+        """
+        Return the class title attr by default,
+        but you can override this method to further customize
+        """
+        return self.title
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = self.get_title()
+        return context
+
+class IndexView(PageTitleViewMixin, TemplateView):
     template_name = "core/index.html"
+    title = "HEMAFRAN ADM"
